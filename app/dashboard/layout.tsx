bc348327c9f615a2718282d1fcefe17274c24930
Salvatore Noto <54959187+salvinoto@users.noreply.github.com>
2025-12-16 23:12:48 -0500

    Refactor DashboardLayout and AppSidebar components for improved layout and consistency
    - Updated the DashboardLayout component to enhance the header styling and remove unnecessary wrapper elements, improving layout consistency.
    - Simplified the AppSidebar component by removing the collapsible property, streamlining its implementation.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -23,10 +23,9 @@ export default function DashboardLayout({
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
+                <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
                         <Breadcrumb>
                             <BreadcrumbList>
                                 <BreadcrumbItem className="hidden md:block">
@@ -40,9 +39,8 @@ export default function DashboardLayout({
                                 </BreadcrumbItem>
                             </BreadcrumbList>
                         </Breadcrumb>
-                    </div>
                 </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="flex flex-1 flex-col gap-4 p-4">
                     {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -172,7 +172,7 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar {...props}>
       <SidebarHeader>
         <TeamSwitcher />
       </SidebarHeader>
